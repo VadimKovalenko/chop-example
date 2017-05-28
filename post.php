210e08421a1611445b84233e582cb0763688a186
Vadim Kovalenko <vadblaxmith@gmail.com>
2017-05-28 18:54:56 +0200

    fixed issue with overriding

--- post.php
+++ post.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	//Grab all data in JSON format
 	//add true as a parament, because use an array
@@ -27,36 +26,5 @@
 				echo "<h4>Brak danych do wyświetlenia</h4>";
 			}
 	}
-
-=======
-<?php
-	//Grab all data in JSON format
-	//add true as a parament, because use an array
-	$user_arr = json_decode(stripcslashes($_POST['data']), true);
-
-	//key is the attr name
-	//value is a value of current input
-	function showFormData($data) {
-			foreach ($data as $arr) {
-				$name = strip_tags(trim($arr['name'])); 
-				$value = strip_tags(trim($arr['value']));
-				if(!empty($value)) {				
-					$parsedata .= "<p>" . $name . " is " . $value . "</p>";
-					$status = true;	
-				} else {
-					$status = false;
-					//if at least one data is empty, break the cycle
-					break;
-				}
-			};
-
-			if($status == true) {
-				echo "<h4>Dziękujemy za wysłanie formularza z następującymi danymi:</h4>";
-				echo $parsedata;
-			} else {
-				echo "<h4>Brak danych do wyświetlenia</h4>";
-			}
-	}
-
->>>>>>> 9f98356ded8103de04631f6dca880a933e3c2867
-	echo showFormData($user_arr);
+	
+echo showFormData($user_arr);
